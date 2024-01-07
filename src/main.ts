6c59e273ea26eba2069f4b76f7ea78e4723d40fc
unknown <tushabajaj10@gmail.com>
2024-01-08 01:22:41 +0530

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,39 @@
 import './css/style.css'
+import FullList from './model/FullList'
+import ListItem from './model/LIstItem'
+import ListTemplate from './Templates/ListTemplate'
 
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault()
+    const input = document.getElementById("newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if(!newEntryText.length) return //not necessary to write this
+
+    const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length-1].id) + 1 : 1
+
+    const newItem = new ListItem(itemId.toString(),newEntryText)
+
+    fullList.addItem(newItem)
+
+    template.render(fullList)
+  })
+
+  const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList() // this is clearing the data - backend
+    template.clear() //this is clearing the list - frontend
+  })
+
+  fullList.load()
+  template.render(fullList)
+
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
--- src/model/LIstItem.ts
+++ src/model/LIstItem.ts
@@ -7,16 +7,32 @@ export interface Item {
 export default class ListItem implements Item {
     constructor(
         private _id: string = '',
-        // private _item: string = '',
-        // private _checked: bool = false,
-    ){}
+        private _item: string = '',
+        private _checked: boolean = false,
+    ) { }
 
-    get id():string {
+    get id(): string {
         return this._id
     }
 
-    set id(id:string){
-        this._id=id
+    set id(id: string) {
+        this._id = id
     }
 
-}{}
+    get item(): string {
+        return this._item
+    }
+
+    set item(item: string) {
+        this._item = item
+    }
+
+    get checked(): boolean {
+        return this._checked
+    }
+
+    set checked(checked: boolean) {
+        this._checked = checked
+    }
+
+} { }
